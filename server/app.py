d191ab81c6652b1b4e2d7c243166f59d03eed4e6
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-14 09:00:47 +0900

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from models import db, User, DictionaryEntry, load_example_sentences
-from utils import generate_sentence, save_sentence
+from models import db, User, Dictionary, DictionaryEntry
+from utils import generate_sentence, save_sentence, load_example_sentences
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dictionary.db'
@@ -19,7 +19,7 @@ def load_user(user_id):
 @app.route('/')
 def index():
     if current_user.is_authenticated:
-        return render_template('index.html')
+        return redirect(url_for('home'))
     else:
         return redirect(url_for('login'))
 
@@ -35,18 +35,18 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST']:
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             login_user(user)
-            return redirect(url_for('index'))
+            return redirect(url_for('home'))
         else:
             flash('ユーザー名またはパスワードが無効です')
     return render_template('login.html')
@@ -57,17 +57,46 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/entries', methods=['GET'])
+@app.route('/home')
 @login_required
-def get_entries():
-    entries = DictionaryEntry.query.filter_by(user_id=current_user.id).all()
+def home():
+    dictionaries = Dictionary.query.filter_by(user_id=current_user.id).all()
+    return render_template('home.html', dictionaries=dictionaries)
+
+@app.route('/create_dictionary', methods=['POST'])
+@login_required
+def create_dictionary():
+    name = request.form['name']
+    new_dictionary = Dictionary(user_id=current_user.id, name=name)
+    db.session.add(new_dictionary)
+    db.session.commit()
+    return redirect(url_for('view_dictionary', dictionary_id=new_dictionary.id))
+
+@app.route('/user/<username>/dict/<int:dictionary_id>')
+@login_required
+def view_dictionary(username, dictionary_id):
+    dictionary = Dictionary.query.get_or_404(dictionary_id)
+    if dictionary.user_id != current_user.id:
+        return redirect(url_for('home'))
+    return render_template('dictionary.html', dictionary=dictionary)
+
+@app.route('/user/<username>/dict/<int:dictionary_id>/entries', methods=['GET'])
+@login_required
+def get_entries(username, dictionary_id):
+    dictionary = Dictionary.query.get_or_404(dictionary_id)
+    if dictionary.user_id != current_user.id:
+        return redirect(url_for('home'))
+    entries = DictionaryEntry.query.filter_by(dictionary_id=dictionary_id).all()
     return jsonify([entry.to_dict() for entry in entries])
 
-@app.route('/entries', methods=['POST'])
+@app.route('/user/<username>/dict/<int:dictionary_id>/entries', methods=['POST'])
 @login_required
-def add_entry():
+def add_entry(username, dictionary_id):
+    dictionary = Dictionary.query.get_or_404(dictionary_id)
+    if dictionary.user_id != current_user.id:
+        return redirect(url_for('home'))
     data = request.json
-    new_entry = DictionaryEntry(user_id=current_user.id, form=data['form'])
+    new_entry = DictionaryEntry(dictionary_id=dictionary_id, form=data['form'])
     db.session.add(new_entry)
     db.session.commit()
     return jsonify(new_entry.to_dict()), 201
@@ -78,20 +107,13 @@ def example_sentences():
     sentences = load_example_sentences()
     return jsonify(sentences)
 
-@app.route('/settings', methods=['GET', 'POST'])
+@app.route('/user/<username>/dict/<int:dictionary_id>/export', methods=['GET'])
 @login_required
-def settings():
-    if request.method == 'GET':
-        return jsonify(get_settings())
-    elif request.method == 'POST':
-        data = request.json
-        update_settings(data)
-        return jsonify(success=True)
-
-@app.route('/export', methods=['GET'])
-@login_required
-def export_json():
-    data = {"words": [entry.to_dict() for entry in DictionaryEntry.query.filter_by(user_id=current_user.id).all()], "version": 2}
+def export_json(username, dictionary_id):
+    dictionary = Dictionary.query.get_or_404(dictionary_id)
+    if dictionary.user_id != current_user.id:
+        return redirect(url_for('home'))
+    data = {"words": [entry.to_dict() for entry in DictionaryEntry.query.filter_by(dictionary_id=dictionary_id).all()], "version": 2}
     return jsonify(data)
 
 if __name__ == '__main__':
